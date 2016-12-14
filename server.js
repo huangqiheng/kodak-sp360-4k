14f4038c00f90bd891475a02552b67f0d761fb80
and <and@hqh.me>
2016-12-15 03:08:13 +0800

    update

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const ptr = require('json-ptr');
 const merge = require('merge');
 
 http.createServer(function (req, res) {
-	fs.createReadStream("index.html").pipe(res)
-}).listen(8080)
+	fs.createReadStream("index.html").pipe(res);
+}).listen(8080);
 
 let server = ws.createServer(function (conn) {
 	conn.client_name = null;
@@ -17,10 +17,9 @@ let server = ws.createServer(function (conn) {
 		if (!req) return;
 		if (!ptr.has(req, '/cmd')) return;
 		if (!ptr.has(req, '/id')) return;
-		if (!ptr.has(req, '/time')) return;
 
-		switch (ptr.get(req, '/cmd')) of {
-		case 'set_info':
+		switch (ptr.get(req, '/cmd')) {
+		case 'set_name':
 			conn.client_name = ptr.get(req, '/client_name');
 			res_ok(conn, req);
 			break;
@@ -28,22 +27,30 @@ let server = ws.createServer(function (conn) {
 			let names = get_names();
 			res_ok(conn, req, {'names': names});
 			break;
-		case 'state_photo_snapshot':
-			break;
-		case 'take_photos':
-			break;
-		case 'state_photo_website':
-			break;
-		case 'get_photos':
-			break;
-		case 'stitch_photos':
-			break;
 		default:
-
+			if (!ptr.has(req, '/target_name')) { 
+				res_err(conn, req, 'unknow command.');
+				return;
+			}
+			let target_conn = get_conn(ptr.get(req, '/target_name'));
+			target_conn || res_err(conn, req, 'unknow target.');
+			target_conn && target_conn.sendText(str);
 		}
 	});
-})
-server.listen(8081)
+});
+
+server.listen(8081);
+
+function res_err(conn, req, err_str) {
+	let res = {};
+	res.status = 'error';
+	res.info = err_str;
+	res.id = req.id;
+	res.time = Date.now();
+	res.req = req;
+	conn.sendText(JSON.stringify(res));
+	return res;
+}
 
 function res_ok(conn, req, data=null) {
 	let res = {};
@@ -62,6 +69,18 @@ function get_names() {
 		conn.client_name && res.push(conn.client_name);
 	});
 	return res;
+}
+
+function get_conn(client_name) {
+	if (!client_name) {
+		return null;
+	}
+	server.conns.forEach(function (conn) {
+		if (client_name == conn.client_name) {
+			return conn;
+		}
+	});
+	return null;
 }
 
 function parse(str) {
