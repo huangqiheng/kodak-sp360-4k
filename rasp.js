fbae9a0e496178aec056dda48dc046197066b3c7
And Huang <huangqiheng@gmail.com>
2017-02-16 10:34:55 +0000

    update

--- rasp.js
+++ rasp.js
@@ -1,8 +1,29 @@
 'use strict';
 
-var Gpio = require('onoff').Gpio,
-  button = new Gpio(22, 'in', 'both');
+require('./config.js');
+var fs  = require('fs');
+var tty = fs.createWriteStream('/dev/tty1');
 
-button.watch(function(err, value) {
-	console.log('button pressed');
+const Gpio = require('onoff').Gpio;
+const  button = new Gpio(5, 'in', 'both');
+
+button.watch((err, value)=> {
+	if (err) {
+		throw err;
+	}
+	if (value) {
+		tft_console('button pressed: ' +  value);
+	}
 });
+
+process.on('SIGINT', ()=> {
+	button.unexport();
+});
+
+function tft_console(msg)
+{
+	console.log(msg);
+	tty.write(msg+'\n');
+}
+
+
